74aceef28a62e2b12d0d92be59bbe9682248b559
David Burke <david@burkesoftware.com>
2019-11-26 22:14:16 -0500

    meh

--- glitchtip/settings.py
+++ glitchtip/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 import os
+import environ
+
+path = environ.Path()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
