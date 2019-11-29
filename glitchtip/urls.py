3db81f01c73315e5440b9c39ced22f08877ae36a
David Burke <david@burkesoftware.com>
2019-11-28 19:11:36 -0500

    urls for SPA

--- glitchtip/urls.py
+++ glitchtip/urls.py
@@ -1,21 +1,9 @@
-"""glitchtip URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/dev/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path, include
 
 urlpatterns = [
     path("api/", include("issues.urls")),
     path("rest-auth/", include("rest_auth.urls")),
+    # These routes belong to the Angular single page app
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^(login|issues).*$', TemplateView.as_view(template_name='index.html')),
 ]
