c9dbdd967be9796e43f081a7b491be2f1b82c771
David Burke <david@burkesoftware.com>
2019-11-28 19:18:38 -0500

    derp

--- glitchtip/urls.py
+++ glitchtip/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path("api/", include("issues.urls")),
     path("rest-auth/", include("rest_auth.urls")),
     # These routes belong to the Angular single page app
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
-    url(r'^(login|issues).*$', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^$', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(login|issues).*$', TemplateView.as_view(template_name='index.html')),
 ]
