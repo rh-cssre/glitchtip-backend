94c4e508d38e63788935bee61f0b70c078487991
David Burke <david@burkesoftware.com>
2020-03-08 10:38:01 -0400

    More csp

--- glitchtip/settings.py
+++ glitchtip/settings.py
@@ -144,7 +144,7 @@ CORS_ORIGIN_ALLOW_ALL = env.bool("CORS_ORIGIN_ALLOW_ALL", True)
 CORS_ORIGIN_WHITELIST = env.tuple("CORS_ORIGIN_WHITELIST", str, default=())
 SECURE_BROWSER_XSS_FILTER = True
 CSP_STYLE_SRC = ["'self'", "'unsafe-inline'"]
-CSP_STYLE_SRC_ELEM = ["'self'", "https://fonts.googleapis.com"]
+CSP_STYLE_SRC_ELEM = ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"]
 CSP_FONT_SRC = ["'self'", "https://fonts.gstatic.com"]
 SECURE_HSTS_SECONDS = env.int("SECURE_HSTS_SECONDS", 0)
 SECURE_HSTS_PRELOAD = env.bool("SECURE_HSTS_PRELOAD", False)
