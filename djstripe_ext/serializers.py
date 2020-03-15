8b55609c3428f82f70bbe580cbea9366ad65f9e9
David Burke <david@burkesoftware.com>
2020-03-15 16:34:33 -0400

    Added plan serializer

--- djstripe_ext/serializers.py
+++ djstripe_ext/serializers.py
@@ -1,7 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+from djstripe.models import Plan
 from djstripe.contrib.rest_framework.serializers import (
     SubscriptionSerializer as BaseSubscriptionSerializer,
 )
 
 
+class PlanSerializer(ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = ("id", "nickname", "amount")
+
+
 class SubscriptionSerializer(BaseSubscriptionSerializer):
-    pass
+    plan = PlanSerializer(read_only=True)
