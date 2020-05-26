309a7bab8ef73d6d8066cf532b07c9fb2c47d29d
Brendan <brendan@burkesoftware.com>
2020-05-26 15:48:20 -0400

    less destructive seed for cypress testing. Also more specific email

--- test_api/views.py
+++ test_api/views.py
@@ -17,6 +17,14 @@ class SeedDataAPIView(APIView):
     def post(self, request):
         if settings.ENABLE_TEST_API is not True:
             raise NotFound("Enable Test API is not enabled")
-        User.objects.all().delete()
-        User.objects.create_user(email="test@example.com", password="hunter22")
+
+        try:
+            test_user = User.objects.get(email="cypresstest@example.com")
+        except User.DoesNotExist:
+            test_user = None
+
+        if test_user:
+            test_user.delete()
+
+        User.objects.create_user(email="cypresstest@example.com", password="hunter22")
         return Response()
