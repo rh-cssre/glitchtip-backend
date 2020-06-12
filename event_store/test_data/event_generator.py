65381cff9955978f7878496a5c7c7d63d42bce31
David Burke <david@burkesoftware.com>
2020-06-12 10:16:17 -0400

    Update locust syntax

--- event_store/test_data/event_generator.py
+++ event_store/test_data/event_generator.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 from . import django_error_factory
 from .csp import mdn_sample_csp
 
+events = django_error_factory.all_django_events
+events.append(mdn_sample_csp)
+
 
 def make_event_unique(event):
     """ Assign event a random new event_id and current timestamp """
@@ -16,9 +19,6 @@ def make_event_unique(event):
 
 def generate_random_event():
     """ Return a random event from library of samples with unique event id """
-
-    events = django_error_factory.all_django_events
-    events.append(mdn_sample_csp)
     event = random.choice(events)
     result = make_event_unique(event)
 
--- locustfile.py
+++ locustfile.py
@@ -1,20 +1,14 @@
-from locust import HttpLocust, TaskSet, between
+from locust import HttpUser, between, task
 from event_store.test_data.event_generator import generate_random_event
 
 
-def send_event(l):
-    project_id = "1"
-    # dsn = "d74b8844b5664a7da945b74c98b8301b"
-    dsn = "28b72f66826d4c818a40455085d23382"
-    event_url = f"/api/{project_id}/store/?sentry_key={dsn}"
-    event = generate_random_event()
-    l.client.post(event_url, json=event)
-
-
-class UserBehavior(TaskSet):
-    tasks = {send_event: 1}
-
-
-class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
+class WebsiteUser(HttpUser):
     wait_time = between(1.0, 2.0)
+
+    @task
+    def send_event(self):
+        project_id = "6"
+        dsn = "244703e8083f4b16988c376ea46e9a08"
+        event_url = f"/api/{project_id}/store/?sentry_key={dsn}"
+        event = generate_random_event()
+        self.client.post(event_url, json=event)
