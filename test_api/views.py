eb839c607acac365371bec046c575f3eb105a96e
Brendan <brendan@burkesoftware.com>
2020-06-23 13:59:09 -0400

    whoops! add this

--- test_api/views.py
+++ test_api/views.py
@@ -7,6 +7,7 @@ from users.models import User
 from organizations_ext.models import Organization
 from teams.models import Team
 from projects.models import Project
+from allauth.account.models import EmailAddress
 
 
 class SeedDataAPIView(APIView):
@@ -22,6 +23,7 @@ class SeedDataAPIView(APIView):
             raise NotFound("Enable Test API is not enabled")
 
         user_email = "cypresstest@example.com"
+        other_user_email = "cypresstest-other@example.com"
         user_password = "hunter22"
         organization_name = "cypresstestorg"
         team_slug = "cypresstestteam"
@@ -29,6 +31,16 @@ class SeedDataAPIView(APIView):
 
         User.objects.filter(email=user_email).delete()
         user = User.objects.create_user(email=user_email, password=user_password)
+
+        User.objects.filter(email=other_user_email).delete()
+        other_user = User.objects.create_user(
+            email=other_user_email, password=user_password
+        )
+
+        EmailAddress.objects.create(user=user, email=user_email, primary=True, verified=False)
+        EmailAddress.objects.create(
+            user=other_user, email=other_user_email, primary=True, verified=False
+        )
 
         Organization.objects.filter(name=organization_name).delete()
         organization = Organization.objects.create(name=organization_name)
