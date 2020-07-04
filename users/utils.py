1501062fa91493511ddc2efe1d40cfeabd804de0
David Burke <david@burkesoftware.com>
2020-07-04 18:57:07 -0400

    Remove token auth (in favor of only session)

--- users/utils.py
+++ users/utils.py
@@ -7,3 +7,8 @@ def is_user_registration_open() -> bool:
     if not enable_user_registration:
         enable_user_registration = not Organization.objects.exists()
     return enable_user_registration
+
+
+def noop_token_creator(token_model, user, serializer):
+    """ Fake token creator to use sessions instead of tokens """
+    return None
