91dc852b4ea81e33909de3dd0eac7be47ead7475
David Burke <david@burkesoftware.com>
2020-07-25 20:18:04 +0000

    GET js for error report embed works

---
+++ user_reports/tests.py
@@ -0,0 +1,41 @@
+import uuid
+from django.shortcuts import reverse
+from model_bakery import baker
+from rest_framework.test import APITestCase
+from glitchtip import test_utils  # pylint: disable=unused-import
+from .models import UserReport
+
+
+class ErrorPageEmbedTestCase(APITestCase):
+    def setUp(self):
+        self.url = reverse("error_page")
+        self.project = baker.make("projects.Project")
+        self.project_key = baker.make("projects.ProjectKey", project=self.project)
+
+    def test_get_not_found(self):
+        res = self.client.get(self.url)
+        # Slight deviation from OSS Sentry as it would 404, but better consistency with DRF
+        self.assertEqual(res.status_code, 400)
+        res = self.client.get(self.url + "?dsn=lol")
+        self.assertEqual(res.status_code, 400)
+
+    def test_get_embed(self):
+        params = {"dsn": self.project_key.get_dsn(), "eventId": uuid.uuid4().hex}
+        res = self.client.get(self.url, params)
+        self.assertContains(res, self.project_key.public_key.hex)
+
+    def test_submit_report(self):
+        params = f"?dsn={self.project_key.get_dsn()}&eventId={uuid.uuid4().hex}"
+        data = {"name": "Test Name", "email": "test@example.com", "comments": "hmm"}
+        res = self.client.post(self.url + params, data)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(UserReport.objects.filter(project=self.project).exists())
+
+    def test_submit_report_with_issue(self):
+        issue = baker.make("issues.Issue", project=self.project)
+        event = baker.make("issues.Event", issue=issue)
+        params = f"?dsn={self.project_key.get_dsn()}&eventId={event.event_id.hex}"
+        data = {"name": "Test Name", "email": "test@example.com", "comments": "hmm"}
+        res = self.client.post(self.url + params, data)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(UserReport.objects.filter(issue=issue).exists())
---
+++ user_reports/views.py
@@ -0,0 +1,126 @@
+import json
+from django.db import IntegrityError
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+from rest_framework import views, exceptions, permissions, renderers
+from rest_framework.response import Response
+
+from projects.models import ProjectKey
+from issues.models import Event
+from .forms import UserReportForm
+from .serializers import ErrorPageEmbedSerializer
+
+
+class JavaScriptSuccessRenderer(renderers.JSONRenderer):
+    """
+    Render as JavaScript when status code is 200.
+    On failure status codes, render as JSON instead.
+    """
+
+    media_type = "application/javascript"
+    format = "js"
+
+    def render(self, data, media_type=None, renderer_context=None):
+        if renderer_context["response"].status_code == 200:
+            charset = "utf-8"
+            return data.encode(charset)
+        return super().render(data, "application/json", renderer_context)
+
+
+class ErrorPageEmbedView(views.APIView):
+    """
+    Ideas taken from OSS Sentry sentry/web/error_page_embed.py
+    Modified for DRF
+    """
+
+    permission_classes = [permissions.AllowAny]
+    renderer_classes = [
+        renderers.JSONRenderer,
+        renderers.BrowsableAPIRenderer,
+        JavaScriptSuccessRenderer,
+    ]
+
+    def get_format_suffix(self, **kwargs):
+        if self.request.method == "GET":
+            return "js"
+        return super().get_format_suffix(**kwargs)
+
+    def process_get_params(self, params):
+        serializer = ErrorPageEmbedSerializer(data=params)
+        serializer.is_valid(raise_exception=True)
+        return serializer.validated_data
+
+    def get_project_key(self, validated_data):
+        dsn = validated_data["dsn"]
+        try:
+            return ProjectKey.from_dsn(dsn)
+        except ProjectKey.DoesNotExist:
+            raise exceptions.NotFound("Invalid dsn parameter")
+
+    def get(self, request):
+        data = self.process_get_params(request.GET)
+        self.get_project_key(data)
+
+        initial = {"name": request.GET.get("name"), "email": request.GET.get("email")}
+
+        # Stubbed, should be configurable
+        show_branding = True
+
+        form = UserReportForm(initial=initial)
+        template = render_to_string(
+            "user_reports/error-page-embed.html",
+            {
+                "form": form,
+                "show_branding": show_branding,
+                "title": data["title"],
+                "subtitle": data["subtitle"],
+                "subtitle2": data["subtitle2"],
+                "name_label": data["labelName"],
+                "email_label": data["labelEmail"],
+                "comments_label": data["labelComments"],
+                "submit_label": data["labelSubmit"],
+                "close_label": data["labelClose"],
+            },
+        )
+
+        url = settings.GLITCHTIP_DOMAIN.geturl() + request.get_full_path()
+
+        context = {
+            "endpoint": mark_safe("*/" + json.dumps(url) + ";/*"),
+            "template": mark_safe("*/" + json.dumps(template) + ";/*"),
+            "strings": mark_safe(
+                json.dumps(
+                    {
+                        "generic_error": str(data["errorGeneric"]),
+                        "form_error": str(data["errorFormEntry"]),
+                        "sent_message": str(data["successMessage"]),
+                    }
+                )
+            ),
+        }
+
+        return Response(
+            render_to_string("user_reports/error-page-embed.js", context, request),
+            content_type="application/javascript",
+        )
+
+    def post(self, request):
+        data = self.process_get_params(request.GET)
+        project_key = self.get_project_key(data)
+        event_id = data["eventId"]
+        initial = {"name": request.GET.get("name"), "email": request.GET.get("email")}
+        form = UserReportForm(request.POST, initial=initial)
+        if form.is_valid():
+            report = form.save(commit=False)
+            report.project = project_key.project
+            report.event_id = event_id
+            event = Event.objects.filter(event_id=event_id).first()
+            if event:
+                report.issue = event.issue
+            try:
+                report.save()
+            except IntegrityError:
+                pass  # Duplicate, ignore
+            return Response()
+        return Response({"errors": form.errors}, status=400)
