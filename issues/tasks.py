b69fb9dea7821aabbced32976a84cf077e8d2c64
David Burke <david@burkesoftware.com>
2020-08-10 21:33:49 -0400

    Use raw delete for stale event

--- issues/tasks.py
+++ issues/tasks.py
@@ -9,5 +9,8 @@ from .models import Event, Issue
 def cleanup_old_events():
     """ Delete older events and associated data  """
     days = settings.GLITCHTIP_MAX_EVENT_LIFE_DAYS
-    Event.objects.filter(created__lt=now() - timedelta(days=days)).delete()
+    # Workaround https://code.djangoproject.com/ticket/9519
+    qs = Event.objects.filter(created__lt=now() - timedelta(days=days))
+    qs._raw_delete(qs.db)
+    # Do not optimize Issue with raw_delete as it has FK references to it.
     Issue.objects.filter(event=None).delete()
--- issues/tests/test_tasks.py
+++ issues/tests/test_tasks.py
@@ -10,10 +10,11 @@ from ..tasks import cleanup_old_events
 
 class TasksTestCase(TestCase):
     def test_cleanup_old_events(self):
-        baker.make("issues.Event")
+        events = baker.make("issues.Event", _quantity=5)
+        baker.make("issues.Event", issue=events[0].issue, _quantity=5)
         cleanup_old_events()
-        self.assertEqual(Event.objects.count(), 1)
-        self.assertEqual(Issue.objects.count(), 1)
+        self.assertEqual(Event.objects.count(), 10)
+        self.assertEqual(Issue.objects.count(), 5)
 
         with freeze_time(
             now() + timedelta(days=settings.GLITCHTIP_MAX_EVENT_LIFE_DAYS)
