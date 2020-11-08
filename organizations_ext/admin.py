b030b036e9a96365efb023ae5b6aabf6fa833e41
David Burke <david@burkesoftware.com>
2020-11-08 15:20:00 -0500

    Add event count to organizations django admin

--- organizations_ext/admin.py
+++ organizations_ext/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Sum
 from organizations.base_admin import (
     BaseOrganizationAdmin,
     BaseOrganizationUserAdmin,
@@ -18,7 +19,18 @@ class OrganizationUserInline(admin.StackedInline):
 
 
 class OrganizationAdmin(BaseOrganizationAdmin):
+    list_display = ["name", "is_active", "is_accepting_events", "event_count"]
     inlines = [OrganizationUserInline, OwnerInline]
+    show_full_result_count = False
+
+    def event_count(self, obj):
+        return obj.event_count
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(event_count=Sum("projects__issue__count"))
+        return queryset
+
 
 
 class OrganizationUserAdmin(BaseOrganizationUserAdmin):
