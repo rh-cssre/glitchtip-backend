02067404dd6b30ff1af0b916f650458f20b1da0b
David Burke <david@burkesoftware.com>
2020-11-22 11:23:38 -0500

    Update dj-stripe

--- glitchtip/test_utils/generators.py
+++ glitchtip/test_utils/generators.py
@@ -1,5 +1,5 @@
 from model_bakery import baker
-from model_bakery.random_gen import gen_slug, gen_datetime, gen_integer, gen_json
+from model_bakery.random_gen import gen_slug, gen_datetime, gen_integer, gen_related
 
 
 def currency_code():
@@ -11,4 +11,4 @@ baker.generators.add("djstripe.fields.StripeCurrencyCodeField", currency_code)
 baker.generators.add("djstripe.fields.StripeIdField", gen_slug)
 baker.generators.add("djstripe.fields.StripeDateTimeField", gen_datetime)
 baker.generators.add("djstripe.fields.StripeQuantumCurrencyAmountField", gen_integer)
-baker.generators.add("django.db.models.JSONField", gen_json)
+baker.generators.add("djstripe.fields.StripeForeignKey", gen_related)
