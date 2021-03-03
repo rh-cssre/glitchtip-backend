9cde1b5ae731e7af4cece9fb715223d1479603e8
David Burke <david@burkesoftware.com>
2021-03-02 20:25:52 -0500

    Update frontend SPA urls

--- glitchtip/urls.py
+++ glitchtip/urls.py
@@ -126,7 +126,7 @@ urlpatterns += [
     # These routes belong to the Angular single page app
     re_path(r"^$", TemplateView.as_view(template_name="index.html")),
     re_path(
-        r"^(auth|login|register|issues|settings|organizations|profile|accept|reset-password).*$",
+        r"^(auth|login|register|(.*)/issues|(.*)/settings|(.*)/performance|organizations|profile|accept|reset-password).*$",
         TemplateView.as_view(template_name="index.html"),
     ),
     # Change the activate_url in the confirm emails
