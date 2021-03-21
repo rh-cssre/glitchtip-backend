024fae4fbc2d58b42c0b2e714210984068584a50
David Burke <david@burkesoftware.com>
2021-03-21 13:49:10 -0400

    Alert recipient serializer

--- alerts/serializers.py
+++ alerts/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import ProjectAlert
+from .models import ProjectAlert, AlertRecipient
+
+
+class AlertRecipientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AlertRecipient
+        fields = ("pk", "recipient_type", "url")
 
 
 class ProjectAlertSerializer(serializers.ModelSerializer):
+    alertrecipient_set = AlertRecipientSerializer(many=True, required=False)
+
     class Meta:
         model = ProjectAlert
-        fields = ("pk", "timespan_minutes", "quantity")
+        fields = ("pk", "timespan_minutes", "quantity", "alertrecipient_set")
