55d07129c4f0746fe681be89f041a857960e4b41
Arnaud de Mouhy <arnaud@admds.net>
2021-04-25 08:54:12 +0200

    Added MAILGUN_API_URL env var

--- glitchtip/settings.py
+++ glitchtip/settings.py
@@ -248,6 +248,7 @@ DEFAULT_FROM_EMAIL = env.str("DEFAULT_FROM_EMAIL", "webmaster@localhost")
 ANYMAIL = {
     "MAILGUN_API_KEY": env.str("MAILGUN_API_KEY", None),
     "MAILGUN_SENDER_DOMAIN": env.str("MAILGUN_SENDER_DOMAIN", None),
+    "MAILGUN_API_URL": env.str("MAILGUN_API_URL", "https://api.mailgun.net/v3")
 }
 
 ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
