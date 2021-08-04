d500042f3c24331d22c76bfd5bfefb5e709b469d
David Burke <david@burkesoftware.com>
2021-08-04 19:49:26 +0000

    Uptime monitoring

---
+++ uptime/tests.py
@@ -0,0 +1,55 @@
+import asyncio
+from unittest import mock
+from aioresponses import aioresponses
+from django.test import TestCase
+from freezegun import freeze_time
+from model_bakery import baker
+from glitchtip import test_utils  # pylint: disable=unused-import
+from .tasks import dispatch_checks
+from .utils import fetch_all
+from .models import Monitor, MonitorCheck
+from .constants import MonitorType
+
+
+class UptimeTestCase(TestCase):
+    @mock.patch("uptime.tasks.perform_checks.run")
+    def test_dispatch_checks(self, mocked):
+        mock.return_value = None
+        test_url = "https://example.com"
+        with freeze_time("2020-01-01"):
+            mon1 = baker.make(Monitor, url=test_url, monitor_type=MonitorType.GET)
+            mon2 = baker.make(Monitor, url=test_url, monitor_type=MonitorType.GET)
+            baker.make(MonitorCheck, monitor=mon1)
+
+        self.assertEqual(mocked.call_count, 2)
+        with freeze_time("2020-01-02"):
+            baker.make(MonitorCheck, monitor=mon2)
+            dispatch_checks()
+        self.assertEqual(mocked.call_count, 3)
+
+    @aioresponses()
+    def test_fetch_all(self, mocked):
+        test_url = "https://example.com"
+        mon1 = baker.make(Monitor, url=test_url, monitor_type=MonitorType.GET)
+        mocked.get(test_url, status=200)
+        loop = asyncio.get_event_loop()
+        monitors = list(Monitor.objects.all().values())
+        results = loop.run_until_complete(fetch_all(monitors, loop))
+        self.assertEqual(results[0]["id"], mon1.pk)
+
+    @aioresponses()
+    def test_monitor_checks_integration(self, mocked):
+        test_url = "https://example.com"
+        mocked.get(test_url, status=200)
+        with freeze_time("2020-01-01"):
+            mon = baker.make(Monitor, url=test_url, monitor_type=MonitorType.GET)
+        self.assertEqual(mon.checks.count(), 1)
+
+        mocked.get(test_url, status=200)
+        with freeze_time("2020-01-01"):
+            dispatch_checks()
+        self.assertEqual(mon.checks.count(), 1)
+
+        with freeze_time("2020-01-02"):
+            dispatch_checks()
+        self.assertEqual(mon.checks.count(), 2)
---
+++ uptime/utils.py
@@ -0,0 +1,57 @@
+import asyncio
+from datetime import timedelta
+from ssl import SSLError
+import time
+import aiohttp
+from .constants import MonitorType, MonitorCheckReason
+
+
+DEFAULT_TIMEOUT = 10
+DEFAULT_PING_TIMEOUT = 1
+DEFAULT_AIOHTTP_TIMEOUT = aiohttp.ClientTimeout(total=DEFAULT_TIMEOUT)
+PING_AIOHTTP_TIMEOUT = aiohttp.ClientTimeout(total=DEFAULT_PING_TIMEOUT)
+
+
+async def process_response(monitor, response):
+    if response.status == monitor["expected_status"]:
+        if monitor["expected_body"]:
+            if monitor["expected_body"] in await response.text():
+                monitor["is_up"] = True
+            else:
+                monitor["reason"] = MonitorCheckReason.BODY
+        else:
+            monitor["is_up"] = True
+    else:
+        monitor["reason"] = MonitorCheckReason.STATUS
+
+
+async def fetch(session, monitor):
+    url = monitor["url"]
+    monitor["is_up"] = False
+    start = time.monotonic()
+    try:
+        if monitor["monitor_type"] == MonitorType.PING:
+            async with session.head(url, timeout=PING_AIOHTTP_TIMEOUT):
+                monitor["is_up"] = True
+        elif monitor["monitor_type"] == MonitorType.GET:
+            async with session.get(url, timeout=DEFAULT_AIOHTTP_TIMEOUT) as response:
+                await process_response(monitor, response)
+        elif monitor["monitor_type"] == MonitorType.POST:
+            async with session.post(url, timeout=DEFAULT_AIOHTTP_TIMEOUT) as response:
+                await process_response(monitor, response)
+        monitor["response_time"] = timedelta(seconds=time.monotonic() - start)
+    except SSLError:
+        monitor["reason"] = MonitorCheckReason.SSL
+    except asyncio.TimeoutError:
+        monitor["reason"] = MonitorCheckReason.TIMEOUT
+    except OSError:
+        monitor["reason"] = MonitorCheckReason.UNKNOWN
+    return monitor
+
+
+async def fetch_all(monitors, loop):
+    async with aiohttp.ClientSession(loop=loop) as session:
+        results = await asyncio.gather(
+            *[fetch(session, monitor) for monitor in monitors], return_exceptions=True
+        )
+        return results
