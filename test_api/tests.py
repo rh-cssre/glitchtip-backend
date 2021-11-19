b6eecacb40e2583a31eac6fafd5010e924ba5ce8
James Kiger <james.kiger@gmail.com>
2021-11-19 12:47:31 -0500

    Adds monitor to data seeding for integration testing

--- test_api/tests.py
+++ test_api/tests.py
@@ -1,6 +1,7 @@
 from django.shortcuts import reverse
 from rest_framework.test import APITestCase
 from users.models import User
+from glitchtip.uptime.models import Monitor
 
 
 class TestAPITestCase(APITestCase):
@@ -10,6 +11,9 @@ class TestAPITestCase(APITestCase):
             res = self.client.post(url)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(User.objects.all().count(), 2)
+
+        monitor = Monitor.objects.all().first()
+        self.assertEqual(monitor.name, "cytestmonitor")
 
     def test_disabled_test_api(self):
         with self.settings(ENABLE_TEST_API=False):
