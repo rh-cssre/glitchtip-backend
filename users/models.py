f5df626f1bdac17eb9db27714f1977188603a085
David Burke <david@burkesoftware.com>
2021-12-07 11:05:50 -0500

    Fix deprecation warnings in prep for Django 4.0

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin,
 )
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class UserManager(BaseUserManager):
