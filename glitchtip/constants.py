7bf00092d55556d757b0f4ccd7e75994fbf48e5e
David Burke <david@burkesoftware.com>
2022-01-28 13:59:18 -0500

    Remove oauth for amazon and okta since we couldn't get them working

--- glitchtip/constants.py
+++ glitchtip/constants.py
@@ -1,7 +1,3 @@
-from allauth.socialaccount.providers.amazon.views import AmazonOAuth2Adapter
-from allauth.socialaccount.providers.amazon_cognito.views import (
-    AmazonCognitoOAuth2Adapter,
-)
 from allauth.socialaccount.providers.digitalocean.views import DigitalOceanOAuth2Adapter
 from allauth.socialaccount.providers.gitea.views import GiteaOAuth2Adapter
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
@@ -9,11 +5,8 @@ from allauth.socialaccount.providers.gitlab.views import GitLabOAuth2Adapter
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.microsoft.views import MicrosoftGraphOAuth2Adapter
 from allauth.socialaccount.providers.nextcloud.views import NextCloudAdapter
-from allauth.socialaccount.providers.okta.views import OktaOAuth2Adapter
 
 SOCIAL_ADAPTER_MAP = {
-    "amazon": AmazonOAuth2Adapter,
-    "amazon_cognito": AmazonCognitoOAuth2Adapter,
     "digitalocean": DigitalOceanOAuth2Adapter,
     "github": GitHubOAuth2Adapter,
     "gitlab": GitLabOAuth2Adapter,
@@ -21,5 +14,4 @@ SOCIAL_ADAPTER_MAP = {
     "microsoft": MicrosoftGraphOAuth2Adapter,
     "gitea": GiteaOAuth2Adapter,
     "nextcloud": NextCloudAdapter,
-    "okta": OktaOAuth2Adapter,
 }
