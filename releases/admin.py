8917a209f9cf560a02786d83005642c861bc1429
James Kiger <james.kiger@gmail.com>
2022-05-03 08:33:25 -0400

    Release admin adjustments

--- releases/admin.py
+++ releases/admin.py
@@ -1,19 +1,9 @@
 from django.contrib import admin
-from django.forms.models import BaseInlineFormSet
 from .models import Release, ReleaseFile
-
-
-class ReleaseFileInlineFormSet(BaseInlineFormSet):
-    def get_queryset(self):
-        if not hasattr(self, "_queryset"):
-            # pylint: disable=attribute-defined-outside-init
-            self._queryset = super().get_queryset()  # noqa
-        return self._queryset
 
 
 class ReleaseFileInlineAdmin(admin.TabularInline):
     model = ReleaseFile
-    formset = ReleaseFileInlineFormSet
     fields = ["file", "name"]
     can_delete = False
 
@@ -28,6 +18,7 @@ class ReleaseAdmin(admin.ModelAdmin):
     search_fields = ["organization__name", "projects__name"]
     list_display = ["version", "organization"]
     list_filter = ["created"]
+    autocomplete_fields = ["organization", "projects"]
     inlines = [ReleaseFileInlineAdmin]
 
 
