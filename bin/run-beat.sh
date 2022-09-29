bf7e3cf494830a1b4eeda5f4404d256f7f9d106a
Vinay Bommana <vbommana@redhat.com>
2022-09-29 20:57:12 +0530

    modified celery scripts

--- bin/run-beat.sh
+++ bin/run-beat.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-exec celery -A glitchtip beat -s /celery/celerybeat-schedule -l info --uid=app --gid=app --pidfile=
+exec celery -A glitchtip beat -s /code/var/run/celerybeat-schedule -l info --uid=app --gid=app --pidfile=
