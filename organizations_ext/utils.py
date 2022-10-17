0861a8de18a731221b52c728202a8da459fb250f
James Kiger <james.kiger@gmail.com>
2022-10-17 15:52:49 -0400

    Simplify registration utils

--- organizations_ext/utils.py
+++ organizations_ext/utils.py
@@ -3,7 +3,4 @@ from organizations_ext.models import Organization
 
 
 def is_organization_creation_open() -> bool:
-    enable_organization_creation = settings.ENABLE_ORGANIZATION_CREATION
-    if not enable_organization_creation:
-        enable_organization_creation = not Organization.objects.exists()
-    return enable_organization_creation
+    return settings.ENABLE_ORGANIZATION_CREATION or not Organization.objects.exists()
--- users/utils.py
+++ users/utils.py
@@ -3,10 +3,7 @@ from users.models import User
 
 
 def is_user_registration_open() -> bool:
-    enable_user_registration = settings.ENABLE_USER_REGISTRATION
-    if not enable_user_registration:
-        enable_user_registration = not User.objects.exists()
-    return enable_user_registration
+    return settings.ENABLE_USER_REGISTRATION or not User.objects.exists()
 
 
 def noop_token_creator(token_model, user, serializer):
