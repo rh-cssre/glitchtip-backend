936d78f6bcfc7a89a3a48a3c63dd8ff2da60d5f5
Christian Assing <cassing@redhat.com>
2023-07-11 08:49:25 +0200

    remove org and project metrics - will be another MR

--- observability/metrics.py
+++ observability/metrics.py
@@ -1,14 +1,11 @@
-from prometheus_client import Counter, Gauge
+from prometheus_client import Counter
 
-organizations_metric = Gauge("glitchtip_organizations", "Number of organizations")
-projects_metric = Gauge(
-    "glitchtip_projects", "Number of projects per organization", ["organization"]
-)
 issues_counter = Counter(
     "glitchtip_issues",
     "Issue creation counter per project",
     ["project", "organization"],
 )
+
 events_counter = Counter(
     "glitchtip_events",
     "Events creation counter per project",
