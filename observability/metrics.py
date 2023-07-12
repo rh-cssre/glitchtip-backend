8eacdfde206da1da013af7e741f73eb5b4a68805
Christian Assing <cassing@redhat.com>
2023-07-12 09:11:42 +0200

    orgs and projects Prometheus metrics
    * glitchtip_organizations - Number of organizations
    * glitchtip_projects - Number of projects per organization
    
    The organization retrieval SQL query (`Organization.objects.all()`) is
    expensive. To avoid any performance degradation, the DB query is cached. The
    cache gets invalidated via organization and project models.

--- observability/metrics.py
+++ observability/metrics.py
@@ -1,5 +1,11 @@
-from prometheus_client import Counter
+from django.core.cache import cache
+from django.db.models import Count
+from prometheus_client import Counter, Gauge
 
+organizations_metric = Gauge("glitchtip_organizations", "Number of organizations")
+projects_metric = Gauge(
+    "glitchtip_projects", "Number of projects per organization", ["organization"]
+)
 issues_counter = Counter(
     "glitchtip_issues",
     "Issue creation counter per project",
@@ -11,3 +17,27 @@ events_counter = Counter(
     "Events creation counter per project",
     ["project", "organization", "issue"],
 )
+
+OBSERVABILITY_ORG_CACHE_KEY = "observability_org_metrics"
+
+
+def compile_metrics():
+    """Update and cache the organization and project metrics"""
+    from organizations_ext.models import Organization  # avoid circular import
+
+    orgs = cache.get_or_set(
+        OBSERVABILITY_ORG_CACHE_KEY,
+        lambda: Organization.objects.annotate(Count("projects"))
+        .values("slug", "projects__count")
+        .all(),
+        timeout=None,
+    )
+
+    for org in orgs:
+        projects_metric.labels(org["slug"]).set(org["projects__count"])
+
+    organizations_metric.set(orgs.count())
+
+
+def clear_metrics_cache():
+    cache.delete(OBSERVABILITY_ORG_CACHE_KEY)
